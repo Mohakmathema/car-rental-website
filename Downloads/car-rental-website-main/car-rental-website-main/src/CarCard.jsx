96362c825c92ddb536e183b30cd507bd7b227f1a
Samrat <np03cs4s240015@heraldcollege.edu.np>
2025-05-04 11:59:15 +0545

    add seller sections

---
+++ Downloads/car-rental-website-main/car-rental-website-main/src/CarCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+import CarCard from './CarCard';
+
+const CarsPage = ({ cars }) => {
+  return (
+    <div className="flex">
+      <Sidebar />
+
+      {/* Offset all content by the sidebar width */}
+      <main className="ml-64 flex-1 p-6 overflow-auto">
+        {cars.map(car => (
+          <CarCard key={car._id} car={car} />
+        ))}
+      </main>
+    </div>
+  );
+};
+
+export default CarsPage;
